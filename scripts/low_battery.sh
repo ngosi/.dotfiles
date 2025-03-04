e39f00a4991fd7e2fd54e551540a02cbc2874a3c
ngosi <ngosi.lind@gmail.com>
2025-03-03 22:29:18 -0400

    Fix low battery warning

--- scripts/low_battery.sh
+++ scripts/low_battery.sh
@@ -4,8 +4,6 @@ LOW_BATTERY_THRESHOLD=2
 BATTERY_LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
 CHARGING_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [[ "$BATTERY_LEVEL" -le "$LOW_BATTERY_THRESHOLD" &&
-      "$(($BATTERY_LEVEL % 5))" -eq 0 &&
-      "$CHARGING_STATUS" != "Charging" ]]; then
+if [[ "$BATTERY_LEVEL" -le "$LOW_BATTERY_THRESHOLD" && "$CHARGING_STATUS" != "Charging" ]]; then
     notify-send -u critical "Low Battery" "Battery level is at $BATTERY_LEVEL%!"
 fi
