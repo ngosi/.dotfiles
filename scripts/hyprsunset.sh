4acfd3e6ea61f22b175fe58258d4e586d0443263
ngosi <ngosi.lind@gmail.com>
2025-03-12 23:05:40 -0400

    Update HyprSunset Service

--- scripts/hyprsunset.sh
+++ scripts/hyprsunset.sh
@@ -4,8 +4,12 @@ killall -q hyprsunset
 
 current_hour=$(date +%H)
 
-if [ "$current_hour" -ge 23 ] || [ "$current_hour" -le 6 ]; then
-    hyprsunset -t 5000
+if [ "$current_hour" -ge 21 ] || [ "$current_hour" -le 6 ]; then
+    if [ "$current_hour" -ge 23 ] || [ "$current_hour" -le 4 ]; then
+        hyprsunset -t 4000
+    else
+        hyprsunset -t 5000
+    fi
 else
     hyprsunset -t 6500
 fi
