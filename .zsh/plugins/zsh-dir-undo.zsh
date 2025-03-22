df0b72a3480d9dc812c6fd29312026296e935202
ngosi <ngosi.lind@gmail.com>
2025-03-21 20:01:47 -0400

    Create UndoDir Zsh Plugin

---
+++ .zsh/plugins/zsh-dir-undo.zsh
@@ -0,0 +1,63 @@
+#!/bin/zsh
+
+undo_dir_hist=($PWD)
+redo_dir_hist=()
+max=16
+skip_hook=0
+
+function quiet_cd() {
+    skip_hook=1
+    zle .kill-buffer
+    cd $1
+    zle .accept-line
+    skip_hook=0
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd on_cwd_change
+function on_cwd_change() {
+    if [[ $skip_hook == 1 || ${undo_dir_hist[-1]} == $PWD ]]; then
+        return
+    fi
+
+    undo_dir_hist+=$PWD
+
+    if [[ $PWD == ${redo_dir_hist[-1]} ]]; then
+        shift -p redo_dir_hist
+    else
+        redo_dir_hist=()
+    fi
+
+    if [[ ${#undo_dir_hist[@]} -gt $max ]]; then
+        shift undo_dir_hist
+    fi
+    # echo "undo: ${undo_dir_hist[@]}\nredo: ${redo_dir_hist[@]}\n"
+}
+
+function undo_dir() {
+    if [[ -n ${undo_dir_hist[@]} ]]; then
+        redo_dir_hist+=${undo_dir_hist[-1]}
+        shift -p undo_dir_hist
+        quiet_cd ${undo_dir_hist[-1]}
+    fi
+    # echo "undo: ${undo_dir_hist[@]}\nredo: ${redo_dir_hist[@]}\n"
+}
+
+function redo_dir() {
+    if [[ -n ${redo_dir_hist[@]} ]]; then
+        undo_dir_hist+=${redo_dir_hist[-1]}
+        shift -p redo_dir_hist
+        quiet_cd ${undo_dir_hist[-1]}
+    fi
+    # echo "undo: ${undo_dir_hist[@]}\nredo: ${redo_dir_hist[@]}\n"
+}
+
+zle -N undo_dir
+zle -N redo_dir
+
+bindkey -M emacs "^o" undo_dir
+bindkey -M vicmd "^o" undo_dir
+bindkey -M viins "^o" undo_dir
+bindkey -M emacs "^i" redo_dir
+bindkey -M vicmd "^i" redo_dir
+bindkey -M viins "^i" redo_dir
