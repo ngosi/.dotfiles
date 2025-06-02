71abe974c03ab5fc51a92cb403aa8dbcfed00854
ngosi <ngosi.lind@gmail.com>
2025-06-01 23:33:38 -0400

    Move Snacks.image images out of the way

--- .config/nvim/lua/allisnull/plugins/snacks.lua
+++ .config/nvim/lua/allisnull/plugins/snacks.lua
@@ -3,10 +3,16 @@ return {
     priority = 1000,
     ---@type snacks.Config
     opts = {
+        styles = {
+            snacks_image = {
+                relative = "editor",
+                col = -1,
+            },
+        },
         image = {
             enabled = true,
             doc = {
-                inline = true,
+                inline = false,
                 float = true,
                 max_width = 60,
                 max_height = 30,
